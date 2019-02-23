87c9ecd81c19bfa2942f085447099ed1d0cb968b
Paige Niedringhaus <paigen11@gmail.com>
2019-02-23 06:24:26 -0800

    whats up github

---
+++ src/components/header.js
@@ -0,0 +1,53 @@
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import logo from '../images/logo.svg';
+
+const Header = () => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          <img src={logo} alt="Level Up Logo" />
+        </Link>
+      </h1>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+};
+
+Header.defaultProps = {
+  siteTitle: ``,
+};
+
+export default Header;
---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from '../components/layout';
+
+export default class About extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quae
+          qui iste, vel dolore aliquam beatae omnis odit a odio dolorem ad iusto
+          dicta rem deserunt impedit tempora eveniet commodi?
+        </p>
+      </Layout>
+    );
+  }
+}
