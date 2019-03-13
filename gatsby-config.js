d23e60942b4a583eede4541c9110dc423f9f312f
Paige Niedringhaus <paigen11@gmail.com>
2019-03-13 16:19:37 -0400

    rewiring for contenful cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,18 +22,12 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        excerpt_separator: `<!-- end -->`,
       },
     },
     `gatsby-plugin-sharp`,
@@ -49,7 +43,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,48 @@
-const path = require('path');
-const { createFilePath } = require('gatsby-source-filesystem');
-const { graphql } = require('gatsby');
+// const path = require('path');
+// const { createFilePath } = require('gatsby-source-filesystem');
+// const { graphql } = require('gatsby');
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === 'MarkdownRemark') {
-    const slug = createFilePath({
-      node,
-      getNode,
-      basePath: 'posts',
-    });
-    createNodeField({
-      node,
-      name: 'slug',
-      value: `/posts${slug}`,
-    });
-  }
-};
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions;
+//   if (node.internal.type === 'MarkdownRemark') {
+//     const slug = createFilePath({
+//       node,
+//       getNode,
+//       basePath: 'posts',
+//     });
+//     createNodeField({
+//       node,
+//       name: 'slug',
+//       value: `/posts${slug}`,
+//     });
+//   }
+// };
 
-exports.createPages = async ({ graphql, actions }) => {
-  await graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      throw result.errors;
-    }
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      actions.createPage({
-        path: node.fields.slug,
-        component: path.resolve(`./src/posts/PostPage.js`),
-        context: {
-          slug: node.fields.slug,
-        },
-      });
-    });
-  });
-};
+// exports.createPages = async ({ graphql, actions }) => {
+//   await graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       throw result.errors;
+//     }
+//     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//       actions.createPage({
+//         path: node.fields.slug,
+//         component: path.resolve(`./src/posts/PostPage.js`),
+//         context: {
+//           slug: node.fields.slug,
+//         },
+//       });
+//     });
+//   });
+// };
